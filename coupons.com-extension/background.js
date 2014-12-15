5bca789b5c78d603d2d13e250570fbf8bbee3cd1
Nikolaos Alexiou <nikolaos.alexiou@gmail.com>
2014-12-14 22:37:23 -0500

    added spinner that fades out

--- coupons.com-extension/background.js
+++ coupons.com-extension/background.js
@@ -1,13 +1,19 @@
 chrome.pageAction.onClicked.addListener(function(tab) {
     chrome.tabs.executeScript(null, { file: "jquery-2.1.1.min.js" }, function() {
+    chrome.tabs.insertCSS(null, {file: "mystyle.css"}, function() {
     chrome.tabs.executeScript(null, { file: "inject.js" });
+     });
 	});
 });
 
-function checkForValidUrl(tabId, changeInfo, tab) {
-if (tab.url.indexOf('http://www.coupons.com') == 0) {
-chrome.pageAction.show(tabId);
-}
+function checkForValidUrl(id, info, tab) {
+    if (tab.status !== "complete"){
+        console.log("not fully loaded yet");
+        return;
+    }
+	if (tab.url.indexOf('http://www.coupons.com') == 0) {
+	chrome.pageAction.show(id);
+	}
 };
 
 chrome.tabs.onUpdated.addListener(checkForValidUrl);
--- coupons.com-extension/inject.js
+++ coupons.com-extension/inject.js
@@ -3,9 +3,18 @@ var loopcount = 0;
 var clippablecoupons = Number($('script').text().match(/clippableTotal\":\{\"count\":(\d\d\d)/)[1])
 
 
+var mydiv = $("<div>", {id: "myloader"});
+mydiv.css('background-image', 'URL(' + chrome.extension.getURL('myspinner.gif') + ')'); 
+var mytext = $("<div>", {text: "Loading coupons...."});
+var coupontag = $("<span>", {id: "mycoupontotal", text: $('.pod.coupon:not(.limited)').length});
+mytext.append('<br/><br/>Total coupons loaded: ').append(coupontag);
+mydiv.html(mytext);
+$('body').append(mydiv);
+
 function scrollBottom() {
 	setTimeout(function timeOut() {
 		var couponsdisplayed = $('.pod.coupon:not(.limited)').length;
+		$('#mycoupontotal').text(couponsdisplayed);
 		window.scrollTo(0,document.body.scrollHeight);
 		
 		if (couponsdisplayed < clippablecoupons) {
@@ -13,7 +22,9 @@ function scrollBottom() {
 			scrollBottom();
 		}
 		else {
-			alert("All clippable coupons (" + couponsdisplayed +") were loaded.");
+			$('#myloader').css('background-image', 'URL(' + chrome.extension.getURL('checkmark.png') + ')');
+			$('div#myloader div').text('Loading complete! ').append('<br/><br/> Total coupons: ' + couponsdisplayed );
+			$('#myloader').fadeOut(6000);
 			window.scrollTo(0,0);	
 		}
 	}, 500);
