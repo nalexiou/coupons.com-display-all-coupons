57eb52f2ab6291d81f1b9353d8abe8758881da55
Nikolaos Alexiou <nikolaos.alexiou@gmail.com>
2014-12-15 23:49:38 -0500

    worked on logic and css for search functionality

--- coupons.com-extension/inject.js
+++ coupons.com-extension/inject.js
@@ -3,19 +3,28 @@ var loopcount = 0;
 var clippablecoupons = Number($('script').text().match(/clippableTotal\":\{\"count\":(\d\d\d)/)[1])
 
 
-if ($('#myloader').length === 0) {
-var mydiv = $("<div>", {id: "myloader"});
-mydiv.css('background-image', 'URL(' + chrome.extension.getURL('myspinner.gif') + ')'); 
-var mytext = $("<div>", {text: "Loading coupons...."});
-var coupontag = $("<span>", {id: "mycoupontotal", text: $('.pod.coupon:not(.limited)').length});
-mytext.append('<br/><br/>Total coupons loaded: ').append(coupontag);
-mydiv.html(mytext);
-$('body').append(mydiv);
-}
+// if ($('#myloader').length === 0) {
+// var mydiv = $("<div>", {id: "myloader"});
+// mydiv.css('background-image', 'URL(' + chrome.extension.getURL('myspinner.gif') + ')'); 
+// var mytext = $("<div>", {text: "Loading coupons...."});
+// var coupontag = $("<span>", {id: "mycoupontotal", text: $('.pod.coupon:not(.limited)').length});
+// mytext.append('<br/><br/>Total coupons loaded: ').append(coupontag);
+// mydiv.html(mytext);
+// $('body').append(mydiv);
+// }
 
-else {
-	$('#myloader').show();
-}
+// else {
+// 	$('#myloader').show();
+// }
+
+var mydiv = $("<div>", {id: "myloader", class: "field"});
+var myinput = $("<input>", {id: "searchterm", placeholder: "Search for coupons", type: "text", class: "field"});
+var mybutton = $("<button>", {id: "search", type: "button"});
+var cancel = $("<button>", {id: "cancel", type: "button"});
+mybutton.text("Find");
+cancel.text("X");
+mydiv.append(myinput, mybutton, cancel);
+$('body').append(mydiv);
 
 function scrollBottom() {
 	setTimeout(function timeOut() {
@@ -39,9 +48,33 @@ function scrollBottom() {
 }
 
 
-scrollBottom();
+// scrollBottom();
 
 
+// $.expr[":"].contains = $.expr.createPseudo(function(arg) {
+//     return function( elem ) {
+//         return $(elem).text().toUpperCase().indexOf(arg.toUpperCase()) >= 0;
+//     };
+// });
+
+
+
+// var keywords = "juice glade";
+// words = keywords.split(/\s+/);
+// wordregex = words.join('|');
+
+// function searchCoupons(arg){
+// 	matchedcoupons = $(".pod.coupon").filter(function() {
+// 	return($(this).text().match(new RegExp(arg, 'i')) != null);
+// 	});
+// 	$.each(matchedcoupons, function(i, item){
+// 		var itemparent = $(item).parent();
+// 		var temp = $(".pod").filter(function() {
+// 			return($(this).text().match(new RegExp(arg, 'i')) == null);
+// 			}).first().replaceWith(item);
+// 		$(itemparent).prepend(temp);
+// 	});
+// }
 
 
 
