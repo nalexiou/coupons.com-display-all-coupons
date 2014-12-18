555e0231fe29db36afd08ca848607447f27d5b54
Nikolaos Alexiou <nikolaos.alexiou@gmail.com>
2014-12-18 10:24:47 -0500

    refactoring and cleanup

--- coupons.com-extension/inject.js
+++ coupons.com-extension/inject.js
@@ -1,93 +1,145 @@
+//setup variables
+
 var clippablecoupons = Number($('script').text().match(/clippableTotal\":\{\"count\":(\d\d\d)/)[1]);
+var clippablecoupons = 50;
 var keywords ="";
 
-if ($('#myloader').length === 0) {
+init();
 
-	$.expr[":"].contains = $.expr.createPseudo(function(arg) {
-	    return function( elem ) {
-	        return $(elem).text().toUpperCase().indexOf(arg.toUpperCase()) >= 0;
-	    };
-	});
+
+function init() {
+
+	if (!formPresent()) {
+		$('body').append(myform);
+		scrollBottom();
+	}
+	else {
+		displayForm();
+	}
+}
+
+function formPresent() {
+
+	if ($('#myloader').length > 0) {
+		return true;
+	}
+
+	else {
+
+		return false;
+	}
+}
+
+//construct form
+function myform(){
 	var mydiv = $("<div>", {id: "myloader"});
 	mydiv.css('background-image', 'URL(' + chrome.extension.getURL('myspinner.gif') + ')'); 
 	var mytext = $("<div>", {id: "toptext", text: "Loading coupons...."});
 	var coupontag = $("<span>", {id: "mycoupontotal", text: $('.pod.coupon:not(.limited)').length});
 	mytext.append('<br/><br/>Total coupons loaded: ').append(coupontag);
-	mydiv.html(mytext);
-	$('body').append(mydiv);
-	scrollBottom();
+
+	return mydiv.append(mytext);
 }
 
-else {
+function displayForm(){
+	//reset form text if keywords were not found
 	if ($('#myloader:contains("not found")').length>0){
-		$('#myloader div h2').text("Enter keywords to display coupons on top of page");
-		$('#myloader div h2').css('color', 'white');
+		var formtext = "Enter keywords to display coupons on top of page";
+		$('#myloader div h2').text(formtext).css('color', 'white');
 	}
+	//display form
 	$('#myloader').show();
+	//remove highlight from coupons
 	$(".pod").css("background-color","inherit");
+}
+
+function displayTotalCoupons(){
+	$('#myloader').css('background-image', 'URL(' + chrome.extension.getURL('checkmark.png') + ')');
+	$('div#myloader div').text('Loading complete! ').append('<br/><br/> Total coupons: ' + couponsdisplayed );
+}
+
+function setupSearchForm(){
+	var searchform ='<div id="myloader"><div><button type="button" id="cancel">X</button><h2>Enter keywords to display coupons on top of page</h2></div><div class="field" id="searchform"><input type="text" id="searchterm" placeholder="Cereal shampoo chocolate Tide Charmin" /><button type="button" id="search">Find</button></div></div>';
+	//replace current form with Search Form
+	$('#myloader').replaceWith(searchform);
+	//setup event listeners for search form
+		$('#cancel').on('click', function(){
+			$('#myloader').hide();
+		});
+		$('#search').on('click', function(){
+			if ($('#searchterm').val() == "") {
+				$('#myloader div h2').text('Please enter a keyword.');
+				$('#myloader div h2').css('color', 'yellow');
+			}
+			else {
+			keywords = $('#searchterm').val().trim();
+			words = keywords.split(/\s+/);
+			wordregex = "\\b("+words.join('|')+")s?\\b";
+			$('#myloader').hide();
+			searchCoupons(wordregex);
+			}
+		});
+
+		$('#searchterm').keypress(function(e) {
+		    if(e.which == 13) {
+		        $('#search').trigger('click');
+		    }
+		});
+		$('#searchterm').on('click', function(){
+			$(this).select();
+		});
+		window.scrollTo(0,0);
 }
 
 function scrollBottom() {
 	setTimeout(function timeOut() {
-		var couponsdisplayed = $('.pod.coupon:not(.limited)').length;
+		//get current coupon count displayed on page
+		couponsdisplayed = $('.pod.coupon:not(.limited)').length;
+		//display current coupon count
 		$('#mycoupontotal').text(couponsdisplayed);
+		//scroll to bottom
 		window.scrollTo(0,document.body.scrollHeight);
 		
+		//recursive call - if all coupons are not displayed, keep scrolling to bottom
 		if (couponsdisplayed < clippablecoupons) {
 			scrollBottom();
 		}
+		//coupons finished loading
 		else {
-			$('#myloader').css('background-image', 'URL(' + chrome.extension.getURL('checkmark.png') + ')');
-			$('div#myloader div').text('Loading complete! ').append('<br/><br/> Total coupons: ' + couponsdisplayed );
-			setTimeout(function(){
-				var myform ='<div id="myloader"><div><button type="button" id="cancel">X</button><h2>Enter keywords to display coupons on top of page</h2></div><div class="field" id="searchform"><input type="text" id="searchterm" placeholder="Cereal shampoo chocolate Tide Charmin" /><button type="button" id="search">Find</button></div></div>';
-				// mydiv.append(myform);
-				$('#myloader').replaceWith(myform);
-				$('#cancel').on('click', function(){
-					$('#myloader').hide();
-				});
-				$('#search').on('click', function(){
-					if ($('#searchterm').val() == "") {
-						$('#myloader div h2').text('Please enter a keyword.');
-						$('#myloader div h2').css('color', 'yellow');
-					}
-					else {
-					keywords = $('#searchterm').val().trim();
-					words = keywords.split(/\s+/);
-					wordregex = "\\b("+words.join('|')+")s?\\b";
-					$('#myloader').hide();
-					searchCoupons(wordregex);
-					}
-				});
-
-				$('#searchterm').keypress(function(e) {
-				    if(e.which == 13) {
-				        $('#search').trigger('click');
-				    }
-				});
-				$('#searchterm').on('click', function(){
-					$(this).select();
-				});
-				window.scrollTo(0,0);
-			}, 3000);
-		}
+			displayTotalCoupons();//display Total Coupon count
+			//setup search form
+			setTimeout(
+				setupSearchForm, 3000);
+			}
 	}, 500);
 }
 
+
+$.expr[":"].matchRegex = $.expr.createPseudo(function(arg) {
+    return function( elem ) {
+    	return $(elem).text().match(new RegExp(arg, 'i')) != null;
+    };
+});
+
+
 function searchCoupons(arg){
-	matchedcoupons = $(".pod.coupon").filter(function() {
-	return($(this).text().match(new RegExp(arg, 'i')) != null);
-	}).css("background-color","yellow");
+	//get matched coupons based on keywords
+	var matchedcoupons = $('.pod.coupon:matchRegex('+arg+')');
+	//set matched coupons to yellow
+	matchedcoupons.css('background-color','yellow');
+	//bubble matched coupons to top
 	if (matchedcoupons.length>0){
 		$.each(matchedcoupons, function(i, item){
 			var itemparent = $(item).parent();
-			var temp = $(".pod").filter(function() {
-				return($(this).text().match(new RegExp(arg, 'i')) == null);
-				}).first().replaceWith(item);
-				$(itemparent).prepend(temp);
-			});
-	window.scrollTo(0,0);
+			var temp = $('.pod:not(:matchRegex('+arg+'))').first().replaceWith(item);
+			// var temp = $(".pod").filter(function() {
+			// 	return($(this).text().match(new RegExp(arg, 'i')) == null);
+			// 	});
+			$(itemparent).prepend(temp);
+		});
+		window.scrollTo(0,0);
 	}
+	//there were no matched coupons. update search form and display it
 	else {
 		$('#myloader div h2').text('\"'+keywords+ '\" not found. Search for something else!');
 		$('#myloader div h2').css('color', 'yellow');
