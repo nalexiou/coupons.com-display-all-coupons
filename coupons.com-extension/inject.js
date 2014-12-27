5907edfd67a000a65795a831790d333f695e266a
Nikolaos Alexiou <nikolaos.alexiou@gmail.com>
2014-12-27 17:15:15 -0500

    a little refactoring

--- coupons.com-extension/inject.js
+++ coupons.com-extension/inject.js
@@ -3,6 +3,7 @@
 var clippablecoupons = Number($('script').text().match(/clippableTotal\":\{\"count\":(\d\d\d)/)[1]);
 var keywords ="";
 var stopexecution = true;
+var clippablecoupons =50;
 var overlay = {
 	    show: function () {
         	$("#lightbox").hide().addClass('spotlight').fadeIn("slow");
@@ -130,12 +131,17 @@ function displayClipButtons(){
 	}
 }
 
+function customBackground(){
+	$('#myloader').css({'background-image': 'URL(' + chrome.extension.getURL('confetti.png') + ')', 'background-repeat': 'repeat'}); 
+	$('div#myloader').css('background-color', 'navy');
+	$('div#myloader h2').css({'background-color': 'navy', 'display': 'inline-block'});	
+}
+
 function setupSearchForm(){
 	var searchform ='<div id="myloader"><div><button type="button" id="cancel">X</button><h2>Enter keywords to display coupons on top of page</h2></div><div class="field" id="searchform"><input type="text" id="searchterm" placeholder="Cereal shampoo chocolate Tide Charmin" /><button type="button" id="search">Find</button></div></div>';
 	//replace current form with Search Form
 	$('#myloader').replaceWith(searchform);
-	$('#myloader').css({'background-image': 'URL(' + chrome.extension.getURL('confetti.png') + ')', 'background-repeat': 'repeat'}); 
-
+	customBackground();
 	var displayclippedelement = $("<button>", {id: "displayclipped", title: "Display clipped coupons"});
 	displayclippedelement.css({'background-image': 'URL(' + chrome.extension.getURL('clip.png') + ')', 'background-repeat': 'no-repeat', 'background-position': 'center center'}); 
 	displayclippedelement.hide();
@@ -144,8 +150,6 @@ function setupSearchForm(){
 	removeclippedelement.css({'background-image': 'URL(' + chrome.extension.getURL('unclip.png') + ')', 'background-repeat': 'no-repeat', 'background-position': 'center center'}); 
 	removeclippedelement.hide();
 	$('#cancel').before(displayclippedelement,removeclippedelement);
-	$('div#myloader').css('background-color', 'navy');
-	$('div#myloader h2').css({'background-color': 'navy', 'display': 'inline-block'});	
 	displayClipButtons();
 	$('#displayclipped').on('click', function(){
 		displayClipped();
